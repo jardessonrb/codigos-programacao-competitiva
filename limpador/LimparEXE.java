36746f74e08b66ffc18ba1de069527eb23451dae
Jardesson <jardessonrs1117@gmail.com>
2022-08-20 11:21:02 -0300

    Atualização no script de limpeza

--- limpador/LimparEXE.java
+++ limpador/LimparEXE.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 
 public class LimparEXE {
 
-    private static void ListaDiretorios(String path){
-        System.out.println(path);
+    private static void ListaDiretorios(String path, String extensao){
         File dir = new File(path);
         if (dir.exists()) {
             File[] files = dir.listFiles();
             if (files.length > 0) {
                 for (int i = 0; i < files.length; i++) {
                     if(files[i].isFile()){
-                        String extensao = files[i].getName().replaceAll("^.*\\.(.*)$", "$1");
-                        if(extensao.equals("exe")){
+                        String extensaoFile = files[i].getName().replaceAll("^.*\\.(.*)$", "$1");
+                        if(extensaoFile.equals(extensao)){
                             System.out.println("Removendo arquivo: "+files[i].getName());
                             files[i].delete();
                         }
                     }else if(files[i].isDirectory()){
-                        ListaDiretorios(path+"\\"+files[i].getName());
+                        ListaDiretorios(path+"\\"+files[i].getName(), extensao);
                     }
                 }
 
@@ -31,8 +30,10 @@ public class LimparEXE {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Caminho da pasta: ");
+        System.out.println("Caminho da pasta: (Ex: C:\\ufpi)");
         String file = input.nextLine();
-        ListaDiretorios(file);
+        System.out.println("Digite a extensão para exclusão(Ex: exe ou json): ");
+        String extensao = input.nextLine();
+        ListaDiretorios(file, extensao);
     }
 }
